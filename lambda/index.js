a28633669c1fa72f56d9c4a0c8a383a9c4cc44e3
CB <ybloc1@outlook.com>
2021-08-09 13:30:53 -0400

    update

--- lambda/index.js
+++ lambda/index.js
@@ -19,6 +19,21 @@ const LaunchRequestHandler = {
     }
 };
 
+const SummaryIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SummaryIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Greetings!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
 const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -144,6 +159,7 @@ const ErrorHandler = {
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
+        SummaryIntentHandler,
         HelloWorldIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
