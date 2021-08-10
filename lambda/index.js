9f3ba85ae8e4fa3a568718a4f79b551cbbb6c3ef
CB <ybloc1@outlook.com>
2021-08-10 15:59:31 -0400

    update

--- lambda/index.js
+++ lambda/index.js
@@ -62,7 +62,7 @@ const SummaryIntentHandler = {
     
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .reprompt()
             .getResponse();
     }
 };
